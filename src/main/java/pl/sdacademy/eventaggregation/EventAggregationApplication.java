6bf22e4c66ff1abb11400cf141fe3c4579a44548
Caliga <artpilsn@gmail.com>
2020-06-06 17:29:30 +0200

    Start

---
+++ src/main/java/pl/sdacademy/eventaggregation/EventAggregationApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.eventaggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAggregationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventAggregationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdacademy/eventaggregation/EventAggregationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.eventaggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventAggregationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
