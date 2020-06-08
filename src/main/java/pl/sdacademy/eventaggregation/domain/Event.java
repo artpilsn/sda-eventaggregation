05ff3595637987ee7764ed8cb4afcbb3c4095146
Enessetere <muszalski.sebastian@gmail.com>
2020-06-08 16:44:08 +0200

    Event entity - added constraints

--- src/main/java/pl/sdacademy/eventaggregation/domain/Event.java
+++ src/main/java/pl/sdacademy/eventaggregation/domain/Event.java
@@ -5,12 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 import static java.util.Objects.nonNull;
@@ -26,13 +29,17 @@ public class Event {
     @GeneratedValue
     private Long idx;
 
+    @NotNull(message = "Title is required")
+    @Length(min = 3, max = 30, message = "Title should have 3 to 30 characters.")
     @Column(name = "title")
     private String title;
 
     @Column(name = "description")
+    @Length(max = 500, message = "Description should be up to 500 characters long.")
     private String description;
 
     //TODO: relation + User object
+    @NotNull(message = "Host cannot be empty.")
     @Column(name = "host")
     private String hostUsername;
 
@@ -46,7 +53,7 @@ public class Event {
     private String address;
 
     @JsonIgnore
-    @AssertTrue(message = "Invalid begin/end of event.")
+    @AssertTrue(message = "Invalid begin/end of event, both fields are required.")
     private boolean isDateValid() {
         return nonNull(from) && nonNull(to)
                 && from.isAfter(LocalDateTime.now())
