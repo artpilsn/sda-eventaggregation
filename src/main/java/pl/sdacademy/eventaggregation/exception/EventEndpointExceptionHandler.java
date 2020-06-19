3b9514d7d8388516932a3b9357b8b2d0f88b9f47
Enessetere <muszalski.sebastian@gmail.com>
2020-06-19 22:05:13 +0200

    ExceptionHandler

---
+++ src/main/java/pl/sdacademy/eventaggregation/exception/EventEndpointExceptionHandler.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.eventaggregation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.sdacademy.eventaggregation.controller.EventApiController;
+import pl.sdacademy.eventaggregation.model.ErrorMessage;
+
+import java.util.List;
+
+@RestControllerAdvice(assignableTypes = EventApiController.class)
+public class EventEndpointExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(EventException.class)
+    public ErrorMessage handleEventException(final EventException exception) {
+        return ErrorMessage.builder()
+                .message("Cannot execute this request.")
+                .details(List.of(exception.getMessage()))
+                .build();
+    }
+
+}
