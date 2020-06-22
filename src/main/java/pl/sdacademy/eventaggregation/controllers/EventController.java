5b97f3bd522e76da9d99c14a240a0b6caeca18f0
Enessetere <muszalski.sebastian@gmail.com>
2020-06-22 16:24:47 +0200

    Fixed events display

--- src/main/java/pl/sdacademy/eventaggregation/controllers/EventController.java
+++ src/main/java/pl/sdacademy/eventaggregation/controllers/EventController.java
@@ -4,21 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sdacademy.eventaggregation.model.EventConverter;
+import pl.sdacademy.eventaggregation.model.EventModel;
+import pl.sdacademy.eventaggregation.model.EventModels;
 import pl.sdacademy.eventaggregation.services.EventCrudService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
 
     private final EventCrudService eventCrudService;
+    private final EventConverter eventConverter;
 
-    public EventController(final EventCrudService eventCrudService) {
+    public EventController(final EventCrudService eventCrudService, final EventConverter eventConverter) {
         this.eventCrudService = eventCrudService;
+        this.eventConverter = eventConverter;
     }
 
     @GetMapping
     public String getAll(final ModelMap modelMap) {
-        modelMap.addAttribute("events", eventCrudService.getAll());
+        modelMap.addAttribute("events", prepareAllEventsToSend());
         return "events";
+    }
+
+    private EventModels prepareAllEventsToSend() {
+        return new EventModels(convertEventListToEventModelList());
+    }
+
+    private List<EventModel> convertEventListToEventModelList() {
+        return eventCrudService.getAll().stream()
+                .map(eventConverter::eventToEventModel)
+                .collect(Collectors.toUnmodifiableList());
     }
 }
